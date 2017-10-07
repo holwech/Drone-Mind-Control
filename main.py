a6c8c348fd22a954c25c2bc16bc65c1594a1f326
Joachim Holwech <joachim.holwech@gmail.com>
2017-10-07 18:55:13 +0200

    Improvements

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from PyQt5.QtWidgets import QApplication
 import pandas as pd
 from lib.stream.stream import Stream
 
+
 if __name__ == '__main__':
     stream = Stream()
     stream.connect()
     time_series = stream.pull_time_series(1000)
     df = pd.DataFrame(time_series)
+
 #    app = QApplication(sys.argv)
 #    ex = GUI()
 #    sys.exit(app.exec_())
-# test
